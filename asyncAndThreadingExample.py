41e33228ab0dd7cbb490a351ea6d476528cf7a17
Alperen Şimşek <alperensimsek.asch@gmail.com>
2026-09-21 10:07:58 +0300

    Asyncio added in project

--- asyncAndThreadingExample.py
+++ asyncAndThreadingExample.py
@@ -1,8 +1,8 @@
 import threading
 import requests
 import time
-
-from main import response
+import asyncio
+import aiohttp
 
 
 def getDataSync(urls):
@@ -46,6 +46,41 @@ def getDataThreading(urls):
         print(f"Çalışma zamanı: {elapsedTime} saniye")
 
 
+async def getDataAsyncButAsWrapper(urls):
+    startTime = time.time()
+    jsonArray = []
+
+    # async yapılan bir işlemin sonucu await olarak alınmalı
+    async with aiohttp.ClientSession() as session:
+        for url in urls:
+            async with session.get(url) as response:
+                jsonArray.append(await response.json())
+
+    endTime = time.time()
+    elapsedTime = endTime - startTime
+    print(f"Çalışma zamanı: {elapsedTime} saniye")
+
+async def getData(session, url, jsonArray):
+    async with session.get(url) as response:
+        jsonArray.append(await response.json())
+
+async def getDataAsyncConcurrently(urls):
+    startTime = time.time()
+    jsonArray = []
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for urls in urls:
+            tasks.append(asyncio.ensure_future(getData(session, urls, jsonArray)))
+        await asyncio.gather(*tasks) # for loop ile içine bilgileri vermek yerine -tasks kullandık
+
+    endTime = time.time()
+    elapsedTime = endTime - startTime
+    print(f"Çalışma zamanı: {elapsedTime} saniye")
+    return jsonArray
+
 urls = ["https://postman-echo.com/delay/3"]*10 # bu sitede 3 saniye gecikme ile çalışır
 #getDataSync(urls) Çalışma zamanı: 33.604796171188354 saniye
 #getDataThreading(urls) Çalışma zamanı: 3.717952013015747 saniye
+#asyncio.run(getDataAsyncButAsWrapper(urls)) Çalışma zamanı: 31.960915327072144 saniye
+#asyncio.run(getDataAsyncConcurrently(urls)) Çalışma zamanı: 3.563718318939209 saniye
