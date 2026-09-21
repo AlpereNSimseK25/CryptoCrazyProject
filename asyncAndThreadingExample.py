3dfef835a946919551d9bb8e92b488624c5e0556
Alperen Şimşek <alperensimsek.asch@gmail.com>
2026-09-21 09:21:05 +0300

    Threading added in project

--- asyncAndThreadingExample.py
+++ asyncAndThreadingExample.py
@@ -2,6 +2,9 @@ import threading
 import requests
 import time
 
+from main import response
+
+
 def getDataSync(urls):
     startTime = time.time() # çalışmaya başladığı zamanı alır
     jsonArray = []
@@ -14,5 +17,34 @@ def getDataSync(urls):
 
     return jsonArray
 
+
+class ThreadingDownloader(threading.Thread):
+
+    jsonArray = []
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+
+    def run(self):
+        response = requests.get(self.url)
+        self.jsonArray.append(response.json())
+        return self.jsonArray
+
+def getDataThreading(urls):
+        startTime = time.time()
+        threads = []
+        for url in urls:
+            thread = ThreadingDownloader(url)
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+            print(thread)
+        endTime = time.time()
+        elapsedTime = endTime - startTime
+        print(f"Çalışma zamanı: {elapsedTime} saniye")
+
 urls = ["https://postman-echo.com/delay/3"]*10 # bu sitede 3 saniye gecikme ile çalışır
 #getDataSync(urls) Çalışma zamanı: 33.604796171188354 saniye
+#getDataThreading(urls) Çalışma zamanı: 3.717952013015747 saniye
